dd3ab9dce34863ee3b60dfcdac180c52d0126c93
RomPop76 <r.popielas@gmail.com>
2022-11-11 17:53:40 +0100

    Commit 2

--- types.js
+++ types.js
@@ -1,22 +1,22 @@
 let hello = 'w"i"taj!'
 // wyświetl w konsoli
-console.log(hello);
+// console.log(hello);
 /* 
 komentarz wieloliniowy
 */
 
 var courseName = 'kurs "JS" na stronie';  //string czyli ciąg tekstowy
-console.log(typeof courseName);
+// console.log(typeof courseName);
 var numberOfPeople = 1200; //number 
-console.log(typeof numberOfPeople);
+// console.log(typeof numberOfPeople);
 var distance = 2.1432432423; //number
 var female = false;  //boolean - prawda/fałsz
-console.log(female, typeof female)
+// console.log(female, typeof female)
 
 var onlyDeclared;  //undefined
 var activeTask = null; // null
-console.log(typeof onlyDeclared);
-console.log(typeof activeTask);
+// console.log(typeof onlyDeclared);
+// console.log(typeof activeTask);
 
 
 var taskArray = [activeTask, 200, "bartek"]; //tablica
@@ -24,5 +24,5 @@ var myNewFunction = function () {
 }; //funkcja
 var myObj = {}; //objekt
 
-console.log(taskArray);
+// console.log(taskArray);
 // sparawdźmy typeof w konsoli
